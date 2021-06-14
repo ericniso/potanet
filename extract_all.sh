572c881ff94da272b9b899207d7b128030f37945
Eric Nisoli <e.nisoli1@campus.unimib.it>
2021-06-14 11:26:37 +0200

    Remove echo print

--- extract_all.sh
+++ extract_all.sh
@@ -3,13 +3,13 @@
 cd model
 
 ROOT_DIR="/data"
-SAMPLES_KIND="tesi"
+SAMPLES_KIND="tesi" # tesi paper
 IMZML_ROI_ROOT_DIR="$ROOT_DIR/imzML_roi_single_spectra_raw"
 IMZML_ROI_EXTRACTED_ROOT_DIR="$ROOT_DIR/imzML_roi_single_spectra_extracted"
 
 mkdir -p "$IMZML_ROI_EXTRACTED_ROOT_DIR"
 
-echo "----- Training"
+# ----- Training
 
 mkdir -p "$IMZML_ROI_EXTRACTED_ROOT_DIR/training"
 
@@ -18,12 +18,11 @@ INPUT="samples/$SAMPLES_KIND/training.txt"
 while IFS= read -r P
 do
   if [ -f "$IMZML_ROI_ROOT_DIR/training/$P.imzML" ] && [ -f "$IMZML_ROI_ROOT_DIR/training/$P.ibd" ]; then
-    # echo $P
     python3 parser.py "$IMZML_ROI_ROOT_DIR/training/$P.imzML" "$IMZML_ROI_EXTRACTED_ROOT_DIR/training/$P"
   fi
 done < "$INPUT"
 
-echo "----- Validation"
+# ----- Validation
 
 mkdir -p "$IMZML_ROI_EXTRACTED_ROOT_DIR/validation"
 
@@ -32,12 +31,11 @@ INPUT="samples/$SAMPLES_KIND/validation.txt"
 while IFS= read -r P
 do
   if [ -f "$IMZML_ROI_ROOT_DIR/validation/$P.imzML" ] && [ -f "$IMZML_ROI_ROOT_DIR/validation/$P.ibd" ]; then
-    # echo $P
     python3 parser.py "$IMZML_ROI_ROOT_DIR/validation/$P.imzML" "$IMZML_ROI_EXTRACTED_ROOT_DIR/validation/$P"
   fi
 done < "$INPUT"
 
-echo "----- Validation exvivo"
+# ----- Validation exvivo
 
 mkdir -p "$IMZML_ROI_EXTRACTED_ROOT_DIR/validation_exvivo"
 
@@ -46,7 +44,6 @@ INPUT="samples/$SAMPLES_KIND/validation_exvivo.txt"
 while IFS= read -r P
 do
   if [ -f "$IMZML_ROI_ROOT_DIR/validation_exvivo/$P.imzML" ] && [ -f "$IMZML_ROI_ROOT_DIR/validation_exvivo/$P.ibd" ]; then
-    # echo $P
     python3 parser.py "$IMZML_ROI_ROOT_DIR/validation_exvivo/$P.imzML" "$IMZML_ROI_EXTRACTED_ROOT_DIR/validation_exvivo/$P"
   fi
 done < "$INPUT"
