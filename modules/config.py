bb5e142b787174e1c050483ef31b46b4ea98fe02
Eric Nisoli <e.nisoli1@campus.unimib.it>
2021-06-18 17:43:01 +0200

    Add preprocessing config variable in env.sh

--- modules/config.py
+++ modules/config.py
@@ -17,6 +17,15 @@ if not "POTANET_SAMPLES_TYPE" in os.environ:
     potanet_logger.error("POTANET_SAMPLES_TYPE missing, it must be set as an environment variable")
     exit(1)
 
+if not "POTANET_PREPROCESSING_BASELINE_MEDIAN" in os.environ:
+    potanet_logger.error("POTANET_PREPROCESSING_BASELINE_MEDIAN missing, it must be set as an environment variable")
+    exit(1)
+
+if not "POTANET_PREPROCESSING_SMOOTHING_MOVING_AVERAGE" in os.environ:
+    potanet_logger.error(
+        "POTANET_PREPROCESSING_SMOOTHING_MOVING_AVERAGE missing, it must be set as an environment variable")
+    exit(1)
+
 POTANET_ROOT_DIR = os.environ.get("POTANET_ROOT_DIR")
 
 POTANET_IMZML_RAW_ROOT_DIR = os.environ.get("POTANET_IMZML_RAW_ROOT_DIR")
@@ -28,3 +37,7 @@ POTANET_THREAD_POOL_SIZE = int(os.environ.get("POTANET_THREAD_POOL_SIZE", 10))
 POTANET_SAMPLES_TYPE = os.environ.get("POTANET_SAMPLES_TYPE")
 
 POTANET_SAMPLES_DIR = "samples"
+
+POTANET_PREPROCESSING_BASELINE_MEDIAN = int(os.environ.get("POTANET_PREPROCESSING_BASELINE_MEDIAN"))
+
+POTANET_PREPROCESSING_SMOOTHING_MOVING_AVERAGE = int(os.environ.get("POTANET_PREPROCESSING_SMOOTHING_MOVING_AVERAGE"))
