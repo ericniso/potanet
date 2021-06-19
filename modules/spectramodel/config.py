6380f41efe1150caee25bfdb190367584f38d7fa
Eric Nisoli <e.nisoli1@campus.unimib.it>
2021-06-19 14:44:41 +0200

    Update configs

--- modules/spectramodel/config.py
+++ modules/spectramodel/config.py
@@ -26,6 +26,16 @@ if not "POTANET_PREPROCESSING_SMOOTHING_MOVING_AVERAGE" in os.environ:
         "POTANET_PREPROCESSING_SMOOTHING_MOVING_AVERAGE missing, it must be set as an environment variable")
     exit(1)
 
+if not "POTANET_PREPROCESSING_NOISE_THRESHOLD" in os.environ:
+    potanet_logger.error(
+        "POTANET_PREPROCESSING_NOISE_THRESHOLD missing, it must be set as an environment variable")
+    exit(1)
+
+if not "POTANET_SPECTRUM_SIZE" in os.environ:
+    potanet_logger.error(
+        "POTANET_SPECTRUM_SIZE missing, it must be set as an environment variable")
+    exit(1)
+
 POTANET_ROOT_DIR = os.environ.get("POTANET_ROOT_DIR")
 
 POTANET_IMZML_RAW_ROOT_DIR = os.environ.get("POTANET_IMZML_RAW_ROOT_DIR")
@@ -41,3 +51,7 @@ POTANET_SAMPLES_DIR = "samples"
 POTANET_PREPROCESSING_BASELINE_MEDIAN = int(os.environ.get("POTANET_PREPROCESSING_BASELINE_MEDIAN"))
 
 POTANET_PREPROCESSING_SMOOTHING_MOVING_AVERAGE = int(os.environ.get("POTANET_PREPROCESSING_SMOOTHING_MOVING_AVERAGE"))
+
+POTANET_PREPROCESSING_NOISE_THRESHOLD = float(os.environ.get("POTANET_PREPROCESSING_NOISE_THRESHOLD"))
+
+POTANET_SPECTRUM_SIZE = int(os.environ.get("POTANET_SPECTRUM_SIZE"))
